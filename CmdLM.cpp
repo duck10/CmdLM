764be811309c15575a30e6be04f72064f5f4167f
larry andrews <larry6640995@gmail.com>
2023-02-06 14:11:18 -0800

    Add project files.

---
+++ CmdLM.cpp
@@ -0,0 +1,72 @@
+
+#include <iostream>
+
+#include "LatticeConverter.h"
+#include "LRL_LatticeMatcher.h"
+#include "LRL_ReadLatticeData.h"
+#include "LRL_ToString.h"
+#include "S6.h"
+
+
+std::vector<S6> GetInputSellingReducedVectors(const std::vector<LRL_ReadLatticeData>& input) {
+   std::vector<S6> v;
+   LatticeConverter converter;
+
+   MatS6 mat;
+   for (size_t i = 0; i < input.size(); ++i) {
+      const S6 s6 = converter.SellingReduceCell(input[i].GetLattice(), input[i].GetCell(), mat);
+      v.push_back(s6);
+   }
+   return v;
+}
+
+std::string ListOutput(const std::vector<S6>& vs,
+   const std::vector<double>& angles,
+   const std::string& referenceLattice) {
+
+   std::ostringstream ostr;
+   for (size_t i2 = 0; i2 < vs.size(); ++i2) {
+      ostr << referenceLattice + "  " << LRL_Cell_Degrees(vs[i2]);
+      ostr << LRL_ToString("(", angles[i2], " degrees)");
+      if (i2 == 0) {
+         ostr << "   REFERENCE";
+      }
+      ostr << std::endl;
+   }
+   return ostr.str();
+}
+
+MatS6 GetMatrixToReturnToReference(const std::string& referenceLattice, const LRL_Cell& cell) {
+   MatS6 mat_primitive_reduced;
+   const LRL_Cell cell_reference = LatticeConverter::SellingReduceCell(referenceLattice, cell, mat_primitive_reduced);
+   return  MatS6::Inverse(mat_primitive_reduced);
+}
+
+int main()
+{
+   std::vector<S6> vLat;
+   std::cout << "; Lattice Matching" << std::endl;
+   const std::vector<LRL_ReadLatticeData> inputList = LRL_ReadLatticeData().ReadLatticeData();
+   if (inputList.empty()) {
+      std::cout << "Lattice match requires something to match to" << std::endl;
+   } else {
+      const std::string referenceLattice = inputList[0].GetLattice();
+      const MatS6 mat_reference = GetMatrixToReturnToReference(referenceLattice, inputList[0].GetCell()); // NOTE: input cell
+
+      vLat = GetInputSellingReducedVectors(inputList);
+
+      LRL_LatticeMatcher lm;
+      lm.SetRecursionDepth(2);
+      lm.SetReferenceLattice(vLat[0]); // Note that this uses the reduced cell
+
+      //lm.PrintMVtree();
+
+      const std::vector<S6> vs = lm.MatchReference(vLat, mat_reference);
+       const std::vector<double> angles(lm.GetAngleAgreements());
+     //for (size_t i = 0; i < vs.size(); ++i) {
+      //   std::cout << i << " " << angles[i] << std::endl;
+      //}
+      std::cout << ListOutput(vs, angles, referenceLattice);
+   }
+
+}
